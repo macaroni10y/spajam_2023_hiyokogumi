80a199e858a3559b837cf89cadcd115507e29cad
kitajima.yuto <kitajima.yuto@furyu.jp>
2023-09-10 16:31:46 +0900

    init firebase project

---
+++ lib/pages/location_sample_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class LocationSamplePage extends StatefulWidget {
+  const LocationSamplePage({Key? key}) : super(key: key);
+
+  @override
+  State<LocationSamplePage> createState() => _LocationSamplePageState();
+}
+
+class _LocationSamplePageState extends State<LocationSamplePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
