12f2e16723f4e44b5b28e63d4e7ac13dfda8c64a
kitajima.yuto <kitajima.yuto@furyu.jp>
2023-09-18 16:40:12 +0900

    use StreamBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:camera/camera.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'pages/home.dart';
 
 Future<void> main() async {
