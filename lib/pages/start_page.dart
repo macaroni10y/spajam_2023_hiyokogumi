1f1a74f857d7c2e579af226f1bfaa7d6f1183c28
kitajima.yuto <kitajima.yuto@furyu.jp>
2023-09-24 02:44:16 +0900

    modify start page layout todo: link

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -1,16 +1,52 @@
 import 'package:flutter/material.dart';
 
-class StartPage extends StatelessWidget {
+import 'agora_page.dart';
 
+class StartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Image.asset('assets/images/animal_mark_hiyoko.png'),
-          Image.asset('assets/images/heyabango@3x.png'),
-          Image.asset('assets/images/heyatsukuru@3x.png'),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Flexible(
+                flex: 4,
+                child: Image.asset('assets/images/animal_mark_hiyoko.png')),
+            Flexible(
+              flex: 1,
+              child: Column(
+                children: [
+                  Container(
+                    width: 150,
+                    margin: EdgeInsets.all(8),
+                    child: GestureDetector(
+                        child: Image.asset('assets/images/heyabango@3x.png'),
+                        onTap: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      const AgoraPage()), // todo replace
+                            )),
+                  ),
+                  Container(
+                    width: 150,
+                    margin: EdgeInsets.all(8),
+                    child: GestureDetector(
+                      child: Image.asset('assets/images/heyatsukuru@3x.png'),
+                      onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                const AgoraPage()), // todo replace
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
