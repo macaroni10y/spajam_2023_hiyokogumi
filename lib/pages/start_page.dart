9d2ff47c269f6113cc79cd637c1a825081222153
kitajima.yuto <kitajima.yuto@furyu.jp>
2023-09-24 04:41:39 +0900

    modify start page layout

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -11,36 +11,26 @@ class StartPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Flexible(
-                flex: 4,
-                child: Image.asset('assets/images/animal_mark_hiyoko.png')),
-            Flexible(
-              flex: 1,
-              child: Column(
-                children: [
-                  Container(
-                    width: 150,
-                    margin: EdgeInsets.all(8),
-                    child: GestureDetector(
-                        child: Image.asset('assets/images/heyabango@3x.png'),
-                        onTap: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    const JoinExistingRoomPage()))),
-                  ),
-                  Container(
-                    width: 150,
-                    margin: EdgeInsets.all(8),
-                    child: GestureDetector(
-                      child: Image.asset('assets/images/heyatsukuru@3x.png'),
-                      onTap: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const CreateNewRoomPage())),
-                    ),
-                  ),
-                ],
+            Image.asset('assets/images/animal_mark_hiyoko.png'),
+            Container(
+              width: 150,
+              margin: EdgeInsets.only(top: 24),
+              child: GestureDetector(
+                  child: Image.asset('assets/images/heyabango@3x.png'),
+                  onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const JoinExistingRoomPage()))),
+            ),
+            Container(
+              width: 150,
+              margin: EdgeInsets.all(16),
+              child: GestureDetector(
+                child: Image.asset('assets/images/heyatsukuru@3x.png'),
+                onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const CreateNewRoomPage())),
               ),
             )
           ],
