86c831cd304072d7706561caa3ff99951d8ab0fe
kitajima.yuto <kitajima.yuto@furyu.jp>
2023-09-23 19:51:18 +0900

    notification sample

--- lib/pages/push_and_notify_page.dart
+++ lib/pages/push_and_notify_page.dart
@@ -10,7 +10,7 @@ class PushAndNotifyPage extends StatefulWidget {
 }
 
 class _PushAndNotifyPageState extends State<PushAndNotifyPage> {
-  Stream<List<Notification>>? _stream;
+  Stream<QuerySnapshot<Map<String, dynamic>>>? _stream;
   Timer? _timer;
   Future<void> _loginAnonymously() async {
     FirebaseAuth.instance.signInAnonymously();
@@ -23,23 +23,29 @@ class _PushAndNotifyPageState extends State<PushAndNotifyPage> {
     super.initState();
   }
 
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+  // .map((snapshot) => snapshot.docs
+  // .where((doc) =>
+  //     doc.data()['toId'] ==
+  //     FirebaseAuth.instance.currentUser?.uid)
+  // .where((element) =>
+  //     element.data()['timestamp'] >
+  //     Timestamp.fromDate(
+  //         DateTime.now().subtract(const Duration(seconds: 10))))
+  //     .map(_convertToNotification)
+  //     .toList());
+
   /// start listening to sleep notification from Firestore
   _startListening() {
-    _timer = Timer.periodic(const Duration(seconds: 3), (timer) {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       setState(() {
         _stream = FirebaseFirestore.instance
             .collection('sleep_notifications')
-            .snapshots()
-            .map((snapshot) => snapshot.docs
-                .where((doc) =>
-                    doc.data()['toId'] ==
-                    FirebaseAuth.instance.currentUser?.uid)
-                .where((element) =>
-                    element.data()['timestamp'] >
-                    Timestamp.fromDate(
-                        DateTime.now().subtract(const Duration(seconds: 10))))
-                .map(_convertToNotification)
-                .toList());
+            .snapshots();
       });
     });
   }
@@ -49,23 +55,42 @@ class _PushAndNotifyPageState extends State<PushAndNotifyPage> {
       Notification(
         fromId: e.data()['fromId'],
         toId: e.data()['toId'],
-        timestamp: e.data()['timestamp'],
+        timestamp: e.data()['timestamp'] ??
+            Timestamp.fromDate(
+                DateTime.now().subtract(const Duration(seconds: 10))),
         message: e.data()['message'],
       );
+
+  Widget _tapped(AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+    List<Notification>? list = snapshot.data?.docs
+        .map(_convertToNotification)
+        .where((e) => e.timestamp
+            .toDate()
+            .isAfter(DateTime.now().subtract(const Duration(seconds: 3))))
+        .toList();
+    return list == null || list.isEmpty
+        ? const Text('no notification')
+        : Column(
+            children: list
+                .map((e) => Text(
+                      e.message,
+                      style: const TextStyle(fontSize: 10),
+                    ))
+                .toList(),
+          );
+  }
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
         child: StreamBuilder(
       stream: _stream,
-      builder:
-          (BuildContext context, AsyncSnapshot<List<Notification>> snapshot) {
+      builder: (BuildContext context,
+          AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
         return Column(
           children: [
             Center(
-              child: CupertinoListTile(
-                title: Text(snapshot.data?.first.message ?? 'no message'),
-              ),
+              child: CupertinoListTile(title: _tapped(snapshot)),
             ),
             Center(
               child: CupertinoListTile(
